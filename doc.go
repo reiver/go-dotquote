bd931cbf789ef0f02cd1734c2bc0e0f0f703e245
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-11-17 23:47:47 -0800

    add some docs

---
+++ doc.go
@@ -0,0 +1,12 @@
+/*
+Package Package dotquote serializes data into the "dotquote" format.
+
+The "dotquote" format is a more human-readable for a list of namespaced key-valued pairs.
+
+The "dotquote" format is also useful for fitting your entire data on a single line of text.
+
+Example
+
+	"app"."name"="myapi" "app"."build"."number"="23" "apple"="one" "banana"="two" "cherry"="three" "trace"."id"="DtehCQqBnw93Tw4h"
+*/
+package dotquote
