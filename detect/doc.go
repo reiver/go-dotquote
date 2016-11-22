f073c94b2f7dd2a6547d41ee421057a49a93b232
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-11-22 07:15:40 -0800

    added some more docs

--- detect/doc.go
+++ detect/doc.go
@@ -1,4 +1,9 @@
 /*
 Package dotquotedetect is a low level library for detecting where keys and values are in dotquote data in a []byte.
+
+This package assumes that the data in the []byte is UTF-8.
+
+Note that UTF-8 was designed to be a superset of ASCII. So something that is just ASCII data is automagically
+also UTF-8 data.
 */
 package dotquotedetect
